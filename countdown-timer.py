16c09650d0b7158320577284ce9476711ffd3fb1
Alex Salmeron <juanasalmeron95@gmail.com>
2024-05-08 12:27:17 -0400

    Countdown timer exercise

---
+++ countdown-timer.py
@@ -0,0 +1,9 @@
+initial_time = int(input("Enter a number: "))
+
+while initial_time > 0:
+    #initial_time -= 1
+    print("There's " + str(initial_time) + " more numbers to go!")
+    initial_time -= 1
+
+print("You have no more numbers left...")
+
